8c073f160b2a6c87f11c01e56b9231457dc22b68
Federico Martin <FM311721@fi365.ort.edu.uy>
2024-09-05 22:07:12 -0300

    Initial commit

---
+++ datastructures/minheap.cpp
@@ -0,0 +1,35 @@
+#ifndef HEAP
+#define HEAP
+#include <cassert>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+class MinHeap
+{
+private:
+    // TODO
+public:
+    MinHeap(int size)
+    {
+        // TODO
+    }
+    void insertar(int valor, int listaDeOrigen)
+    {
+        // TODO
+    }
+    int valorTope() {
+        // TODO
+        return 0;
+    }
+    int listaDeOrigenTope() {
+        // TODO
+        return 0;
+    }
+    void eliminarTope() {
+        // TODO
+    }
+};
+
+#endif
