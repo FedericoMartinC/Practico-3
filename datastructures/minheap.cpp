27a06879de892044e6ce805957474c7af28d9cbf
Federico Martin <FM311721@fi365.ort.edu.uy>
2024-09-10 20:39:48 -0300

    Codigo

--- datastructures/minheap.cpp
+++ datastructures/minheap.cpp
@@ -5,31 +5,93 @@
 #include <iostream>
 using namespace std;
 
-
+template <class T>
 class MinHeap
 {
-private:
-    // TODO
-public:
-    MinHeap(int size)
-    {
-        // TODO
-    }
-    void insertar(int valor, int listaDeOrigen)
-    {
-        // TODO
-    }
-    int valorTope() {
-        // TODO
-        return 0;
-    }
-    int listaDeOrigenTope() {
-        // TODO
-        return 0;
-    }
-    void eliminarTope() {
-        // TODO
-    }
-};
+    private:
+        int capacidad;
+        int primerLibre;
+        T* arr;
+        int (*comparacion)(T, T);
 
+        void swap(int pos1, int pos2) {
+            T aux = arr[pos1];
+            arr[pos1] = arr[pos2];
+            arr[pos2] = aux;
+        }
+
+        int padre(int i) {
+            return i / 2;
+        }
+
+        int izq(int i) {
+            return 2 * i;
+        }
+
+        int der(int i) {
+            return (2 * i) + 1;
+        }
+
+        void flotar(int pos) {
+            if(pos == 1) return;
+            int posPadre = padre(pos);
+            if(comparacion(arr[pos], arr[posPadre]) > 0) {
+                swap(pos, posPadre);
+                flotar(posPadre);
+            }
+        }
+
+        void hundir(int pos) {
+            int posIzq = izq(pos);
+            int posDer = der(pos);
+            if(posIzq < primerLibre) { //  si tengo hijo izquierdo
+                int posMenor = posIzq;
+                // si tengo hijo derecho y mi hijo derecho es menor que mi hijo izquierdo
+                if(posDer < primerLibre && comparacion(arr[posIzq], arr[posDer]) < 0) { 
+                    posMenor = posDer;
+                }
+
+                // si el mejor de mis hijos es menor que yo, intercambio
+                if(comparacion(arr[pos], arr[posMenor]) < 0) { 
+                    swap(pos, posMenor);
+                    hundir(posMenor);
+                }
+            }
+        }
+    public:
+
+        MinHeap(int _capacidad, int (*_comparacion)(T, T)) {
+            capacidad = _capacidad;
+            primerLibre = 1;
+            arr = new T[capacidad + 1]();
+            comparacion = _comparacion;
+        }
+
+        void insertar(T el) {
+            assert(!estaLleno());
+            arr[primerLibre] = el;
+            primerLibre++;
+            flotar(primerLibre - 1);
+        }
+
+        T tope() {
+            assert(!estaVacio());
+            return arr[1];
+        }
+
+        void eliminar() {
+            assert(!estaVacio());
+            swap(1, primerLibre - 1);
+            primerLibre--;
+            hundir(1);
+        }
+
+        bool estaLleno() {
+            return primerLibre > capacidad;
+        }
+
+        bool estaVacio() {
+            return primerLibre == 1;
+        }
+};
 #endif
--- main.cpp
+++ main.cpp
@@ -5,8 +5,72 @@
 #include "./datastructures/minheap.cpp"
 using namespace std;
 
+class datoDoble{
+    private:
+        int num;
+        int pos;
+    public:
+        int getNum(){
+            return this->num;
+        }
+        int getPos(){
+            return this->pos;
+        }
+        void setNum(int num){
+            this->num=num;
+        }
+        void setPos(int pos){
+            this->pos=pos;
+        }
+
+};
+
+int comparar(datoDoble* a,datoDoble* b){
+    return  b->getNum() - a->getNum();
+}
+
 int main()
 {
+    int k;
+    cin >> k;
+    MinHeap<datoDoble*> * prin = new MinHeap<datoDoble*>(k,comparar);
+    int** aux= new int*[k];
+    int* posAc= new int[k]();
+    for (int i=0;i<k;i++){
+        posAc[i]=0;
+    }
+    int* maxPos= new int[k]();
+    for (int i=0;i<k;i++){
+        int p;
+        cin >> p;
+        int* arr= new int[p]();
+        for(int j=0;j<p;j++){
+            int num;
+            cin >> num;
+            if(j==0){
+                datoDoble* nuevo=new datoDoble;
+                nuevo->setPos(i);
+                nuevo->setNum(num);
+                prin->insertar(nuevo);
+            }
+            arr[j]=num;
+        }
+        aux[i]=arr;
+        maxPos[i]=p-1;
+    }
+    while (!prin->estaVacio())
+    {
+        datoDoble* dato =prin->tope();
+        cout << dato->getNum() << endl;
+        prin->eliminar();
+        if(maxPos[dato->getPos()]> posAc[dato->getPos()]){
+            posAc[dato->getPos()]++;
+            datoDoble* nuevo =new datoDoble;
+            nuevo->setNum(aux[dato->getPos()][posAc[dato->getPos()]]);
+            nuevo->setPos(dato->getPos());
+            prin->insertar(nuevo);
+        }
+    }
     
     return 0;
 }
